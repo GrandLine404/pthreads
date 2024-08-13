797285fcb2455b56921c11144bfd0ed4dcc96062
charith jayarangana <cj.ptsz@yahoo.com>
2024-08-13 19:14:27 +0530

    feat: count time for execution in prog1 program

--- prog1.c
+++ prog1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #define ARRAYSIZE 1000
 #define THREADS 2
@@ -26,11 +27,10 @@ void *slave(void *myid)
 	// add locl sum to global sum
 	sum += myresult;
 	pthread_mutex_unlock(&mutex);
-
-	return;
 }
 
-main() {
+void main(int argc, char *argv[]) {
+	struct timeval start,end;
 	int i;
 	pthread_t tid[THREADS];
 	// initialize mutex
@@ -42,6 +42,7 @@ main() {
 	for (i=0; i<ARRAYSIZE;i++)
 		data[i] = i+1;
 
+	gettimeofday(&start, NULL);
 	for (i=0;i<THREADS;i++)
 		if (pthread_create(&tid[i],NULL,slave,(void *)i) != 0)
 			perror("Pthread create fails");
@@ -50,6 +51,12 @@ main() {
 	for (i=0;i<THREADS;i++)
 		if (pthread_join(tid[i],NULL) != 0)
 			perror("Pthread_join_fails");
+	gettimeofday(&end, NULL);
+
+	long seconds = end.tv_sec - start.tv_sec;
+	long microseconds = end.tv_usec - start.tv_usec;
+	double elapsed = seconds*1.0 + microseconds*1e-6;
 
 	printf("The sum from 1 to %i is %d\n",ARRAYSIZE,sum);
+	printf("Execution time: %.6f seconds\n", elapsed);
 }
